b57663b4b080a1f7d02be16e12e3992bec90d37b
FSMM32768 <zhujj_wow@163.com>
2020-12-29 21:07:36 +0800

    modify run.py

--- pdfToJson/run.py
+++ pdfToJson/run.py
@@ -2,11 +2,20 @@ import json
 from pdfClient import grobid_client
 
 
-if __name__ == "__main__":
-    pdf_path = '/home/xjw/pdfToJson/hw/example.pdf'
+def get_content(pdf_path)
     config_path = './config.json'
     pdfClient = grobid_client(config_path=config_path)
     jsonData = pdfClient.process_pdf(pdf_path) #for use
-    
-    with open('example.json','w',encoding='utf8') as f:
-        json.dump(jsonData,f)
+    return jsonData
+
+
+if __name__ == "__main__":
+    pdf_path = '/home/xjw/pdfToJson/hw/example.pdf'
+    jsonData = get_content(pdf_path)
+
+    title = jsonData["title"]
+    authors = jsonData["authors"]
+    keywords = jsonData["keywords"]
+    abstract = jsonData["abstract"]
+    paperContent = jsonData["paperContent"]
+    references = jsonData["references"]
